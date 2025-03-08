3ac6658f10ff6d929c59bdc1a94807a37ecdbf6f
Ingo <you@example.com>
2025-03-08 22:03:57 +0100

    Add Logger

--- client/main.gd
+++ client/main.gd
@@ -2,25 +2,27 @@ extends Node
 
 const packets := preload("res://packets.gd")
 
+@onready var _log: Log = $Log
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
-		print("got input", event.as_text())
+		_log.info("got input %s" % event.as_text())
 		var packet := packets.Packet.new()
 		var chat_msg := packet.new_chat()
 		chat_msg.set_msg("My Event "+event.as_text())
 			
 		var err := WS.send(packet)
 		if err:
-			print("error sending")
+			_log.error("Error sending packet")
 		else:
-			print("sent packet")
+			_log.success("Sent packet")
 
 func _ready() -> void:
 	WS.connected_to_server.connect(_on_ws_connected_to_server)
 	WS.connection_closed.connect(_on_ws_connection_closed)
 	WS.packet_received.connect(_on_ws_packet_received)
 	
-	print("Connecting to server...")
+	_log.info("Connecting to server...")
 	WS.connect_to_url("ws://127.0.0.1:8080/ws")
 
 func _on_ws_connected_to_server() -> void:
@@ -30,12 +32,12 @@ func _on_ws_connected_to_server() -> void:
 	
 	var err := WS.send(packet)
 	if err:
-		print("Error sending packet")
+		_log.error("Error sending packet")
 	else:
-		print("Sent packet")
+		_log.success("Sent packet")
 	
 func _on_ws_connection_closed() -> void:
-	print("Connection closed")
+	_log.error("Connection closed")
 	
 func _on_ws_packet_received(packet: packets.Packet) -> void:
-	print("Received packet from the server: %s" % packet)
+	_log.info("Received packet from the server: %s" % packet)
