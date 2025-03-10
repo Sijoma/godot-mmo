1632ee2f8e53b3cc3f018c22f668f4139e3b7221
Simon Zengerling <simon.zengerling@camunda.com>
2025-03-10 22:52:17 +0100

    feat(server): player loop

--- server/internal/server/states/ingame.go
+++ server/internal/server/states/ingame.go
@@ -1,9 +1,12 @@
 package states
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"math"
 	"math/rand/v2"
+	"time"
 
 	"github.com/sijoma/godot-mmo/internal/server"
 	"github.com/sijoma/godot-mmo/internal/server/objects"
@@ -14,6 +17,8 @@ type InGame struct {
 	client server.ClientInterfacer
 	player *objects.Player
 	logger *log.Logger
+
+	cancelPlayerUpdateLoop context.CancelFunc
 }
 
 func (g *InGame) Name() string {
@@ -41,8 +46,65 @@ func (g *InGame) OnEnter() {
 }
 
 func (g *InGame) HandleMessage(senderId uint64, message packets.Msg) {
+	switch message := message.(type) {
+	case *packets.Packet_Player:
+		g.handlePlayer(senderId, message)
+	case *packets.Packet_PlayerDirection:
+		g.handlePlayerDirection(senderId, message)
+	}
+}
+
+func (g *InGame) handlePlayer(senderId uint64, message *packets.Packet_Player) {
+	if senderId == g.client.Id() {
+		g.logger.Println("Received player message from our own client, ignoring")
+		return
+	}
+	g.client.SocketSendAs(message, senderId)
 }
 
 func (g *InGame) OnExit() {
+	if g.cancelPlayerUpdateLoop != nil {
+		g.cancelPlayerUpdateLoop()
+	}
 	g.client.SharedGameObjects().Players.Remove(g.client.Id())
+}
+
+func (g *InGame) syncPlayer(delta float64) {
+	newX := g.player.X + g.player.Speed*math.Cos(g.player.Direction)*delta
+	newY := g.player.Y + g.player.Speed*math.Sin(g.player.Direction)*delta
+
+	g.player.X = newX
+	g.player.Y = newY
+
+	updatePacket := packets.NewPlayer(g.client.Id(), g.player)
+	g.client.Broadcast(updatePacket)
+	go g.client.SocketSend(updatePacket)
+}
+
+func (g *InGame) playerUpdateLoop(ctx context.Context) {
+	const delta float64 = 0.05
+	ticker := time.NewTicker(time.Duration(delta*1000) * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			g.syncPlayer(delta)
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (g *InGame) handlePlayerDirection(senderId uint64, message *packets.Packet_PlayerDirection) {
+	if senderId == g.client.Id() {
+		g.player.Direction = message.PlayerDirection.Direction
+
+		// If this is the first time receiving a player direction message from our client, start the player update loop
+		if g.cancelPlayerUpdateLoop == nil {
+			ctx, cancel := context.WithCancel(context.Background())
+			g.cancelPlayerUpdateLoop = cancel
+			go g.playerUpdateLoop(ctx)
+		}
+	}
 }
